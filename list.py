3dc7a738084f7695f008b31946694cc29fe2c4fe
Sumit Sahaje <ssahaje@gmail.com>
2021-03-20 00:34:17 +0530

    list

--- list.py
+++ list.py
@@ -1,10 +1,18 @@
 names = ['Sumit', 'Sahaje']
 
 for name in names:
+    print(name)
+    # chars=''
+    # for char in name:
+    #     # print(char)
+    #     chars += char
+    # print(chars)
+
+for name in names:
     # print(name)
     chars=''
     for char in name:
         # print(char)
         chars += char
-        print(chars)
+    print(chars)
 
